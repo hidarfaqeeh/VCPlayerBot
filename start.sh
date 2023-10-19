0ee96a36c48103f8b719497e4c8dc6919529ea43
hidarfaqeeh <hidarfaqeeh@gmail.com>
2023-10-19 19:12:32 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@ echo "Cloning Repo...."
 if [ -z $BRANCH ]
 then
   echo "Cloning main branch...."
-  git clone https://github.com/subinps/VCPlayerBot /VCPlayerBot
+  git clone https://github.com/hidarfaqeeh/VCPlayerBot
 else
   echo "Cloning $BRANCH branch...."
   git clone https://github.com/subinps/VCPlayerBot -b $BRANCH /VCPlayerBot
